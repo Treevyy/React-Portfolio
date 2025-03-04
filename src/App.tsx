fdb38b4d2588e7ee75cdd605bf728ca0beea3f60
Treevyy <aarontrevormoore@gmail.com>
2025-03-04 10:53:05 -0600

    created starter code

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import PortfolioContainer from './components/PortfolioContainer'
+
+function App() {
+  
+  return (
+   <>
+   <PortfolioContainer />
+   </>
+  )
+}
+
+export default App
