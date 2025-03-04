7ce8addb1ce77dce4a2a28cb3a4b9b03ab115bec
Treevyy <aarontrevormoore@gmail.com>
2025-03-04 12:39:48 -0600

    added the project Weather Dashboard to the portfolios tab

--- src/components/pages/Portfolio.tsx
+++ src/components/pages/Portfolio.tsx
@@ -1,20 +1,53 @@
+import Project from '../Project';
+
+const projectData = [
+    {
+      title: "Weather Dashboard",
+      image: "/images/weather-dashboard.png",
+      github: "https://github.com/Treevyy/Weather-Dashboard",
+      deployed: "https://weather-dashboard-h6tx.onrender.com"
+    },
+    {
+      title: "Project 2",
+      image: "https://via.placeholder.com/150",
+      github: "https://github.com",
+      deployed: "https://deployed.com"
+    },
+    {
+      title: "Project 3",
+      image: "https://via.placeholder.com/150",
+      github: "https://github.com",
+      deployed: "https://deployed.com"
+    },
+    {
+      title: "Project 4",
+      image: "https://via.placeholder.com/150",
+      github: "https://github.com",
+      deployed: "https://deployed.com"
+    },
+    {
+      title: "Project 5",
+      image: "https://via.placeholder.com/150",
+      github: "https://github.com",
+      deployed: "https://deployed.com"
+    },
+    {
+      title: "Project 6",
+      image: "https://via.placeholder.com/150",
+      github: "https://github.com",
+      deployed: "https://deployed.com"
+    },
+]
+
 export default function Portfolio() {
     return (
       <div>
         <h1>Portfolio Page</h1>
-        <p>
-          Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-          molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-          magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-          efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-          mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-          posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-          faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-          ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-          dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-          rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-        </p>
+        <div className= "project-container">
+       {projectData.map((project, index) => (
+         <Project key={index} title={project.title} image={project.image} github={project.github} deployed={project.deployed} />
+        ))}
+        </div>
       </div>
     );
   }
