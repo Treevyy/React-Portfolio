fa85e35ed193d8fddad50a26daa6b5ee8f2f1d97
Treevyy <aarontrevormoore@gmail.com>
2025-03-05 01:15:57 -0600

    added more projects to my portfolio page

--- src/components/pages/Portfolio.tsx
+++ src/components/pages/Portfolio.tsx
@@ -8,28 +8,28 @@ const projectData = [
       deployed: "https://weather-dashboard-h6tx.onrender.com"
     },
     {
-      title: "Project 2",
-      image: "https://via.placeholder.com/150",
-      github: "https://github.com",
-      deployed: "https://deployed.com"
+      title: "Blackjack",
+      image: "/images/blackjack.png",
+      github: "https://github.com/leviking12/Blackjack-Project",
+      deployed: "https://leviking12.github.io/Blackjack-Project/"
     },
     {
-      title: "Project 3",
-      image: "https://via.placeholder.com/150",
-      github: "https://github.com",
-      deployed: "https://deployed.com"
+      title: "README File Generator",
+      image: "/images/readme-file-generator.png",
+      github: "https://github.com/Treevyy/README-File-Generator",
+      deployed: "https://app.screencastify.com/v2/manage/videos/pVpg6GD7UZQbECq1Ynsb"
     },
     {
-      title: "Project 4",
-      image: "https://via.placeholder.com/150",
-      github: "https://github.com",
-      deployed: "https://deployed.com"
+      title: "Vehicle Builder",
+      image: "/images/vehicle-builder.png",
+      github: "https://github.com/Treevyy/Vehicle-Builder",
+      deployed: "https://drive.google.com/file/d/1Qw5u79WscYuHcdxuzzfyLglho5YIKbpW/view"
     },
     {
-      title: "Project 5",
-      image: "https://via.placeholder.com/150",
-      github: "https://github.com",
-      deployed: "https://deployed.com"
+      title: "Employee Tracker",
+      image: "/images/employee-tracker.png",
+      github: "https://github.com/Treevyy/Employee-Tracker",
+      deployed: "https://drive.google.com/file/d/1Yy7Q0Lm3xgsRqM9El3YWAKM819-TaMqM/view"
     },
     {
       title: "Project 6",
