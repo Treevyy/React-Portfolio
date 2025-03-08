a43133372e1199b8b0745e3e95b5969fa8b325bc
Treevyy <aarontrevormoore@gmail.com>
2025-03-08 14:29:27 -0600

    debugged main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
         element: <Portfolio />,
       },
       {
-        path: '/Blog',
+        path: '/Contact',
         element: <Contact />,
       },
       {
