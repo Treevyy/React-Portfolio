c409064765bebdca4c6ec81d5d9cb924fdddbf51
Treevyy <aarontrevormoore@gmail.com>
2025-03-08 15:12:25 -0600

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
 // import Footer from './components/Footer'
-import PortfolioContainer from './components/portfoliocontainer'
+// import PortfolioContainer from './components/portfoliocontainer'
+import { Outlet } from 'react-router-dom'
+import Nav from './components/NavTabs'
 
 function App() {
   
   return (
    <>
-   <PortfolioContainer />
+   <Nav />
+   <main className="mx-3">
+   <Outlet />
+    </main>
    </>
   )
 }
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,28 +1,28 @@
-type ProjectProps = {
-    title: string;
-    image: string;
-    github: string;
-    deployed: string;
-};
+// type ProjectProps = {
+//     title: string;
+//     image: string;
+//     github: string;
+//     deployed: string;
+// };
 
-export default function Project({ title, image, github, deployed }: ProjectProps) {
+// export default function Project({ title, image, github, deployed }: ProjectProps) {
 
 
-    return (
-        //   <div className="project">
-        //   <img src={image}></img>
-        //     <h1>{title}</h1>
-        //     <a href={github}>Github</a>
-        //     <br></br>
-        //     <a href={deployed}>Deployed</a>
-        //   </div>
-        <div className="card project">
-            <img src={image} className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <a href={github} target="_blank" className="btn btn-primary me-2">Github</a>
-                <a href={deployed} target="_blank" className="btn btn-primary">Deployed</a>
-            </div>
-        </div>
-    );
-}
+//     return (
+//         //   <div className="project">
+//         //   <img src={image}></img>
+//         //     <h1>{title}</h1>
+//         //     <a href={github}>Github</a>
+//         //     <br></br>
+//         //     <a href={deployed}>Deployed</a>
+//         //   </div>
+//         <div className="card project">
+//             <img src={image} className="card-img-top" alt="..." />
+//             <div className="card-body">
+//                 <h5 className="card-title">{title}</h5>
+//                 <a href={github} target="_blank" className="btn btn-primary me-2">Github</a>
+//                 <a href={deployed} target="_blank" className="btn btn-primary">Deployed</a>
+//             </div>
+//         </div>
+//     );
+// }
