392809ebc9bef7abbe4def84292974b8da0463cb
Treevyy <aarontrevormoore@gmail.com>
2025-03-08 14:26:15 -0600

    set up the main.jsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>Error occurred</div>,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Blog',
+        element: <Contact />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
+  <StrictMode >
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
