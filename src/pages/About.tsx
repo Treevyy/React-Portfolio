0c8044f8fcc9b56bdf6e8dd2ee1430bb0d8e5833
Treevyy <aarontrevormoore@gmail.com>
2025-03-08 19:28:34 -0600

    added a bio on the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,17 +1,95 @@
 export default function About() {
     return (
       <div>
-        <h1>About Page</h1>
+        <h1 style={{marginBottom: '20px'}}>About Me</h1>
+
+        <section id="Bio">
+          <h2>Bio</h2>
         <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Hi, I'm Trevor Moore, a student in the EDX SMU Full-Stack Web Developer Bootcamp with a deep passion for web development. At 32 years old and born and raised in Dallas, Texas. My spouse and I have been together for 15 years, we have two beautiful kids together, one is our spirited 3-year-old daughter and the other is our adorable 4-month-old son. I am driven by the desire to build a new and better life in the tech industry for our family.
         </p>
+        </section>
+        
+        <section id="Professional Background">
+        <h2>Professional Background</h2>
+        <p>
+        I come from a hands-on background in labor, having worked in both oilfield operations and warehouse environments. Determined to create a brighter future for my kids, I transitioned my career focus toward tech. I am currently enrolled in an intensive 6-month Full-Stack Web Development bootcamp that covers:
+          <ul>
+            <li>HTML, CSS and Javascript</li>
+            <li>Web APIs and Node.js</li>
+            <li>TypeScript, Server and APIs, and SQL</li>
+            <li>React, NoSQL, and the MERN stack</li>
+            <li>Authentication, Testing, Deployment and CI/CD</li>
+            <li>Python and Object-Oriented Programming with Python</li>
+          </ul>
+        </p>
+        </section>
+
+        <section id="Mission & Values">
+        <h2>Mission & Values</h2>
+        <p>
+        My mission is to leverage my diverse life experiences to innovate and solve real-world problems through technology. I believe in the power of hard work, resilience, and continuous learning. I value creativity, authenticity, and a commitment to growth, both personally and professionally.
+        </p>
+        </section>
+
+        <section id="Technical Skills & Expertise">
+        <h2>Technical Skills & Expertise</h2>
+        <p>
+        As I continue to build my career in web development, my current tech stack includes:
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>Node.js</li>
+            <li>Server and APIs</li>
+            <li>SQL</li>
+            <li>React</li>
+          </ul>
+          While I'm still at the beginning of my journey in tech, I'm excited to expand my expertise and explore new technologies as I grow.
+        </p>
+        </section>
+
+        <section id="Projects & Achievements">
+        <h2>Projects & Achievements</h2>
+        <p>
+        Throughout my bootcamp and personal projects, I’ve embraced challenges that sharpen my problem-solving skills and fuel my passion for building intuitive, responsive web applications. Although I’m just starting out, every project is a stepping stone toward mastering web development and creating meaningful, real-world solutions.
+        </p>
+        </section>
+
+        <section id="Education & Certifications">
+        <h2>Education & Certifications</h2>
+        <p>
+        I hold an Associate of Science degree, and I'm on track to graduate with a certificate of completion from SMU after my 6-month bootcamp. I’m committed to continuous growth and lifelong learning, always seeking new opportunities to expand my skills and knowledge.
+        </p>
+        </section>
+
+        <section id="Personal Interests & Hobbies">
+        <h2>Personal Interests & Hobbies</h2>
+        <p>
+        When I'm not coding, I enjoy a variety of activities that keep me energized and inspired:
+          <ul>
+            <li>Playing video games</li>
+            <li>Putting together puzzles</li>
+            <li>Watching movies</li>
+            <li>Watching Sports some of my favorites are Football, Soccer, Hockey and Baseball</li>
+            <li>Listening to music</li>
+            <li>Working out</li>
+            <li>Traveling</li>
+            <li>Spending quality time with my family</li>
+          </ul>
+        </p>
+        </section>
+
+        <section id="Let’s Connect">
+        <h2>Let’s Connect</h2>
+        <p>
+        I'm excited to connect with like-minded professionals and tech enthusiasts. Feel free to reach out to me on LinkedIn or check out my projects on GitHub. Whether you're interested in collaboration, want to discuss new ideas, or simply want to say hello, I'd love to hear from you!
+        </p>
+        </section>
+
         <footer className="footer-links">
          <a href= "https://github.com/Treevyy"><i className="fa-brands fa-github"></i></a>
+         <a href= "https://www.linkedin.com/in/aaron-trevor-moore/"><i className="fa-brands fa-linkedin"></i></a>
       </footer>
       </div>
     );
