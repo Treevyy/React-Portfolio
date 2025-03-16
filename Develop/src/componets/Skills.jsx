81fdf770e58673df5edc36c47242be0307563bd3
Treevyy <aarontrevormoore@gmail.com>
2025-03-16 18:04:35 -0500

    Added a skills page and images for the technologies listed

--- Develop/src/componets/Skills.jsx
+++ Develop/src/componets/Skills.jsx
@@ -1,11 +1,68 @@
 import React from 'react'
 
-
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import JavaScript from '../assets/javascript.png'
+import Node from '../assets/node.png'
+import Typescript from '../assets/typescript.png'
+import PostgreSQL from '../assets/postgresql.png'
+import ReactImg from '../assets/react.png'
+import Sequelize from '../assets/sequelize.png'
+import Mango from '../assets/mongo.png'
+import GitHub from '../assets/github.png'
 
 const Skills = () => {
   return (
-    <div>
-      
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-[#8c52ff]'>Skills</p>
+                <p className='py-4'>These are the technologies I've worked with</p>
+            </div>
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={HTML} alt="HTML icon" />
+                    <p className='my-4'>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={CSS} alt="HTML icon" />
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={JavaScript} alt="HTML icon" />
+                    <p className='my-4'>JavaScript</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Node} alt="HTML icon" />
+                    <p className='my-4'>Node.js</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Typescript} alt="HTML icon" />
+                    <p className='my-4'>TypeScript</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={PostgreSQL} alt="HTML icon" />
+                    <p className='my-4'>PostgresSQL</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={ReactImg} alt="HTML icon" />
+                    <p className='my-4'>React</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Sequelize} alt="HTML icon" />
+                    <p className='my-4'>Sequelize</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Mango} alt="HTML icon" />
+                    <p className='my-4'>Mongo DB</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={GitHub} alt="HTML icon" />
+                    <p className='my-4'>GitHub</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
