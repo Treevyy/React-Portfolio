475f8fe5a8175e1597102675a489dd9ea6253b0a
Treevyy <aarontrevormoore@gmail.com>
2025-03-16 03:16:20 -0500

    Created tabs for links and started home componet for the portfolio

--- Develop/src/App.jsx
+++ Develop/src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from "./componets/Navbar"
-
+import Home from "./componets/Home.jsx"
 
 function App() {
 
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   )
 }
