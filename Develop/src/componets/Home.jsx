18f47b322259cd7b1f9c5ae7d3592ca6d2ae3692
Treevyy <aarontrevormoore@gmail.com>
2025-03-21 01:41:33 -0500

    Mobile landscape mode test #2

--- Develop/src/componets/Home.jsx
+++ Develop/src/componets/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
 
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <div className='w-full max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
           <p className='text-[#ccd6f6]'>Hi, my name is</p>
           <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Trevor Moore</h1>
           <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Web Developer</h2>
