deab109a14a9efcab3f4f109a4462dc3e3a3c643
Treevyy <aarontrevormoore@gmail.com>
2025-03-22 05:12:09 -0500

    Changed photo settings to show up on all divices

--- Develop/src/componets/Home.jsx
+++ Develop/src/componets/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
           <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Web Developer </h2>
           <p className='text-[#8892b0] py-4 max-w-700px'>"Every line of code builds the future for tomorrow." 
           </p>
-          <div className='flex-shrink-0 mt-8 md:mt-0' style={{ marginLeft: '25rem' }}>
+          <div className='flex-shrink-0 mt-8 md:mt-0 md:ml-8'>
               <img src={profilepic} alt="Picture of me leaning on the front of my car" className='rounded-full w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 object-cover' />
             </div>
         </div>
